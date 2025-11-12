af0857176ba5b24415637049b7989d1c6259999d
mdalmas <mdalmashossenarise@gmail.com>
2025-11-12 11:51:24 +0600

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:secret_app/pages/register_page.dart';
 import 'package:secret_app/themes/light_mode.dart';
+import 'firebase_options.dart';
 import 'pages/login_page.dart';
 import 'auth/login_or_register.dart';
 void main() {
+   Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
